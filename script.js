5d84a76f91f5f588d8c3b221aa03da12f4e05117
mtomardev <120013813+mtomardev@users.noreply.github.com>
2023-10-26 11:42:51 +0530

    first

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 //your code here
+function swapTheme(){
+    let app = document.getElementById("app");
+    // let swapbutton = document.getElementById("swap")
 
+    if(app.classList.contains("day")){
+        app.classList.remove("day");
+        app.classList.add("night");
+        
+
+    }
+    else{
+        app.classList.remove("night");
+        app.classList.add("day");
+    }
+} 
