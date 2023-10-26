aef0c3b74d4e1dc74b8d954607eb5ba0c32d527b
mtomardev <120013813+mtomardev@users.noreply.github.com>
2023-10-26 11:53:28 +0530

    button

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 //your code here
 function swapTheme(){
     let app = document.getElementById("app");
-    // let swapbutton = document.getElementById("swap")
+    let swapbutton = document.getElementById("swap")
 
     if(app.classList.contains("day")){
         app.classList.remove("day");
         app.classList.add("night");
-        
+        swapbutton.classList.remove("button_day")
+        swapbutton.classList.add("button_night")    
 
     }
     else{
         app.classList.remove("night");
         app.classList.add("day");
+        swapbutton.classList.remove("button_night")
+        swapbutton.classList.add("button_day")
     }
 } 
